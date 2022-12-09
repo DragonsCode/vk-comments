e01174d36383e015f84265c2172572db9ccfc9ad
DragonsCode <84837099+DragonsCode@users.noreply.github.com>
2022-12-09 21:37:03 +0300

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,6 @@
+from .activate import activate_labeler
+from .read_posts import read_post_labeler
+from .add_post import add_post_labeler
+# Если использовать глобальный лейблер, то все хендлеры будут зарегистрированы в том же порядке, в котором они были импортированы
+
+__all__ = ("activate_labeler", "read_post_labeler", "add_post_labeler")
