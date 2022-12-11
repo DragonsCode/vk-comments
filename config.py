c91c1a0ea547619b854cb0c9f2013f62bc884e90
DragonsCode <84837099+DragonsCode@users.noreply.github.com>
2022-12-11 17:17:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from vkbottle.bot import BotLabeler
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
-token = 'vk1.a.PdBxBthx3qG98nynYuoZb1fs0ns80iF_KAOfQDpAgIBQMD1zuC6iBheqpWH0YdoiwGkBeTzCoaiQS_xZmCcw5M_e7R7eiFdhquoceizjJdOYC7JzJaeZEn5Xn5ZgiRJnUht__phyFcRVViif9bDAbt9Q1YBKRZTi6CiWMPAg-EkY0FyD8qGV73ZRnvdkT3_N'
+token = 'vk1.a.c_8cOaawjgcTkTFwmYtRpvJfQ6m09io0ru8EPxiQVt7FJR4lf-5XBgCCRiryyE2yAwtZ6rtd8l4XZG1FGgdoeXECTQgcENbxeXSlgau6GGHAo7uiPGlAePf_scriC-UQRbVsDa8Pr-2xrUL1gV8uJKkPy_Vov6LW0nWeY5MnJfMPNC1gNGdEqXuOOjzMhBlbupjzqC9y2LdNIJfoco5s7A'
 
 api = API(token)
 labeler = BotLabeler()
