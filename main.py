83d8705c406a16e55f07de59c80238cbe57abf9b
DragonsCode <84837099+DragonsCode@users.noreply.github.com>
2022-12-12 15:10:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ bot = Bot(
     state_dispenser=state_dispenser,
 )
 
+@bot.on.private_message(text=['Hello <name>', 'Hello'])
+async def hello(message: Message, name=None):
+    if name is not None:
+        await message.answer(f'Hello to {name}')
+    else:
+        user = await bot.api.users.get(message.from_id)
+        await message.answer(f'Hello {user[0].first_name} {user[0].last_name}')
 
 @bot.on.private_message()
 async def chat(message: Message):
